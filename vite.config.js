be3862f9e31acc75ec492b0fb36c2542929116a5
Avilash <sahaavilash5055@gmail.com>
2025-12-08 18:33:37 +0600

    .....

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-     server: {
+    server: {
         host: '0.0.0.0',
-        hmr: {
-            host: '192.168.1.104', // your PC's local IP
-        },
+        port: 5173,
     },
     plugins: [
         laravel({
